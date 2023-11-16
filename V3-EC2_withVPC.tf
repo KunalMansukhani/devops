74ef25a7fff032be0b748dd64510022779b81162
KunalMansukhani <kdmansukhani@gmail.com>
2023-11-15 23:07:47 -0800

    changes to VPC

--- V3-EC2_withVPC.tf
+++ V3-EC2_withVPC.tf
@@ -1,3 +1,42 @@
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-06e4ca05d431835e9"
+    instance_type = "t2.micro"
+    key_name = "dpp"
+    # security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.dpw-public_subent_01.id
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpw-vpc.id
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+
+  }
+}
 resource "aws_vpc" "dpw-vpc" {
     cidr_block = "10.1.0.0/16"
     tags = {
@@ -9,7 +48,7 @@ resource "aws_subnet" "dpw-public_subent_01" {
     vpc_id = aws_vpc.dpw-vpc.id
     cidr_block = "10.1.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "us-east-1a"
+    availability_zone = "us-west-1a"
     tags = {
         Name = "dpw-public_subent_01"
     }
